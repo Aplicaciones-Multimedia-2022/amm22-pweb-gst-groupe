da8dc8e567bd41d2a8a885c6e3059d03dacc3539
pablo <100346346@alumnos.uc3m.es>
2022-03-24 13:44:47 +0100

    ya funciona

--- Pestañas 3.js
+++ Pestañas 3.js
@@ -1,35 +1,208 @@
-function easyTabs() {
-    var groups = document.querySelectorAll('.pestañas');
-    if (groups.length > 0) {
-      for (i = 0; i < groups.length; i++) {
-        var tabs = groups[i].querySelectorAll('.pest');
-        for (t = 0; t < tabs.length; t++) {
-          tabs[t].setAttribute("index", t+1);
-          if (t == 0) tabs[t].className = "pest selected";
-        }
-        var contents = groups[i].querySelectorAll('.contenido');
-        for (c = 0; c < contents.length; c++) {
-          contents[c].setAttribute("index", c+1);
-          if (c == 0) contents[c].className = "contenido selected";
-        }
-      }
-      var clicks = document.querySelectorAll('.pest');
-      for (i = 0; i < clicks.length; i++) {
-        clicks[i].onclick = function() {
-          var tSiblings = this.parentElement.children;
-          for (i = 0; i < tSiblings.length; i++) {
-            tSiblings[i].className = "t-tab";
-          }
-          this.className = "pest selected";
-          var idx = this.getAttribute("index");
-          var cSiblings = this.parentElement.parentElement.querySelectorAll('.contenido');
-          for (i = 0; i < cSiblings.length; i++) {
-            cSiblings[i].className = "contenido";
-            if (cSiblings[i].getAttribute("index") == idx) {
-              cSiblings[i].className = "contenido selected";
-            }
-          }
-        };
-      }
+
+window.onload = function () {
+  var nav = document.getElementById("main");
+  tabs = nav.getElementsByTagName("span");
+  sections = document.getElementsByTagName("section");
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", activateTab);
+    tabs[1].addEventListener("click", startGame,);
+  
+  }
+
+}
+
+
+function activateTab() {
+  for (var i = 0; i < tabs.length; i++) {
+    var tabClasses = tabs[i].classList;
+    var sectionClasses = sections[i].classList;
+    if (tabs[i] == this) { //Visualize clicked tab
+      tabClasses.add("currentab");
+      sectionClasses.add("current");
+    } else { //Remove previous active tab
+      tabClasses.remove("currentab");
+      sectionClasses.remove("current");
     }
   }
+}
+
+//Aqui empieza el javascript del Juego
+var myGamePiece;
+var myScore;
+var myObstacles = [];
+var muro=[];
+function startGame() {
+    myGamePiece = new component(70, 70, "https://raw.githubusercontent.com/Aplicaciones-Multimedia-2022/amm22-pweb-gst-groupe/main/hogsqueel_alldir_x2_and_actual.webp%20(2).gif", 359, 250, "image");
+    myScore = new component("30px", "Consolas", "black", 350, 40, "text");
+    myGameArea.start();
+}
+
+var myGameArea = {
+  start: function () {
+    var mycanvas = document.getElementById("myCanvas");
+    
+    mycanvas.width = 900;
+    mycanvas.height = 500;
+    this.frameNo = 0;
+    clearInterval(this.interval);
+
+    this.interval = setInterval(updateGameArea, 1);
+    window.addEventListener('keydown', function (e) {
+      myGameArea.key = e.keyCode;
+    })
+    window.addEventListener('keyup', function (e) {
+      myGameArea.key = false;
+    })
+  },
+  clear: function () {
+    var mycanvas = document.getElementById("myCanvas");
+var context = mycanvas.getContext('2d');
+    context.clearRect(0, 0, mycanvas.width, mycanvas.height);
+  },
+  stop: function () {
+    clearInterval(this.interval);
+
+  }
+}
+
+function component(width, height, color, x, y, type) {
+  
+  this.type = type;
+  if (type == "image") {
+    this.image = new Image();
+    this.image.src = color;
+  }
+  this.width = width;
+  this.height = height;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.x = x;
+  this.y = y;
+  this.update = function () {
+    var mycanvas = document.getElementById("myCanvas");
+    var context = mycanvas.getContext('2d');
+    if (type == "image") {
+      context.drawImage(this.image,
+        this.x,
+        this.y,
+        this.width, this.height);
+    } else if (this.type == "text") {
+      context.font = this.width + " " + this.height;
+      context.fillStyle = color;
+      context.fillText(this.text, this.x, this.y);
+    }
+    else {
+      context.fillStyle = color;
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+  this.newPos = function () {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+  this.crashWith = function (otherobj) {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + (otherobj.width);
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + (otherobj.height);
+    var crash = true;
+    if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+      crash = false;
+    }
+    return crash;
+  }
+  
+}
+
+function updateGameArea() {
+
+  for (i = 0; i < myObstacles.length; i += 1) {
+    if (myGamePiece.crashWith(myObstacles[i]) && everyinterval(200)) {
+      return;
+    }
+  }
+
+    
+  
+  myGameArea.clear();
+  myGameArea.frameNo += 1;
+  if (myGameArea.frameNo == 1 || everyinterval(110)) {
+    xobstacle = Math.floor(Math.random() * 880);
+    yobstacle = Math.floor(Math.random() * 480);
+    myObstacles.push(new component(40, 40, "https://cdn-icons-png.flaticon.com/512/32/32128.png", xobstacle, yobstacle, "image"));
+  }
+  if (myGameArea.frameNo == 1 || everyinterval(10)) {
+    xobstacle1 = Math.floor(Math.random() * 880);
+    yobstacle1 = Math.floor(Math.random() * 480);
+    muro.push(new component(40, 40, "red", xobstacle1, yobstacle1,));
+  }
+  for (i = 0; i <17; i += 1) {
+    muro[i].update();
+
+  }
+  for (i = 0; i < myObstacles.length; i += 1) {
+    myObstacles[i].update();
+    myObstacles = myObstacles.slice(i);
+
+  }
+  myScore.text = "SCORE: " + myGameArea.frameNo;
+  myScore.update();
+  myGamePiece.newPos();
+  myGamePiece.update();
+  clearmove()
+  if (myGameArea.key && myGameArea.key == 37) { moveleft() }
+  if (myGameArea.key && myGameArea.key == 39) { moveright() }
+  if (myGameArea.key && myGameArea.key == 38) { moveup() }
+  if (myGameArea.key && myGameArea.key == 40) { movedown() }
+
+}
+
+
+function everyinterval(n) {
+  if ((myGameArea.frameNo / n) % 1 == 0) { return true; }
+  return false;
+}
+
+function moveup() {
+
+  myGamePiece = new component(70, 70, "https://raw.githubusercontent.com/Aplicaciones-Multimedia-2022/amm22-pweb-gst-groupe/main/hogsqueel_alldir_x2_and_actual.webp%20(1).gif", myGamePiece.x, myGamePiece.y, "image");
+  myGamePiece.speedY = -1;
+
+}
+
+function movedown() {
+  myGamePiece = new component(70, 70, "https://raw.githubusercontent.com/Aplicaciones-Multimedia-2022/amm22-pweb-gst-groupe/main/hogsqueel_alldir_x2_and_actual.webp%20(2).gif", myGamePiece.x, myGamePiece.y, "image");
+
+  myGamePiece.speedY = 1;
+}
+
+function moveleft() {
+  myGamePiece = new component(70, 70, "https://raw.githubusercontent.com/Aplicaciones-Multimedia-2022/amm22-pweb-gst-groupe/main/hogsqueel_alldir_x2_and_actual.webp.gif", myGamePiece.x, myGamePiece.y, "image");
+
+  myGamePiece.speedX = -1;
+}
+
+function moveright() {
+  myGamePiece = new component(70, 70, "https://raw.githubusercontent.com/Aplicaciones-Multimedia-2022/amm22-pweb-gst-groupe/main/hogsqueel_alldir_x2_and_actual.webp%20(3).gif", myGamePiece.x, myGamePiece.y, "image");
+
+  myGamePiece.speedX = 1;
+}
+
+function clearmove() {
+  myGamePiece.speedX = 0;
+  myGamePiece.speedY = 0;
+}
+function clickPlayagain() {
+  reset();
+  startGame();
+
+}
+function reset() {
+  myGamePiece = undefined;
+  myObstacles = [];
+  myScore = "";
+}
