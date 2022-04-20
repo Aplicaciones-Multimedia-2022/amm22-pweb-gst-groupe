63494520bcc9a2be03b0ee89e18b1145e7adb3bf
100429180 <100429180@alumnos.uc3m.es>
2022-04-20 15:56:22 +0200

    15_55

---
+++ p2_1.js
@@ -0,0 +1,8 @@
+
+const navToogle = document.querySelector('#navToogle');
+const nav = document.querySelector('nav');
+
+navToogle.addEventListener("click", () => {//when the navToogle is click do sth
+    nav.classList.toogle('open')//when I click open the nav 
+}
+)
