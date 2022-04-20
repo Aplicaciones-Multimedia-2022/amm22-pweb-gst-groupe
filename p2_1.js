f90777bece3ef4aea1123ad9b28c40e516afe69a
Thestickyfinger <100682504+Thestickyfinger@users.noreply.github.com>
2022-04-21 01:26:20 +0200

    Delete p2_1.js

--- p2_1.js
+++
@@ -1,8 +0,0 @@
-
-const navToogle = document.querySelector('#navToogle');
-const nav = document.querySelector('nav');
-
-navToogle.addEventListener("click", () => {//when the navToogle is click do sth
-    nav.classList.toogle('open')//when I click open the nav 
-}
-)
